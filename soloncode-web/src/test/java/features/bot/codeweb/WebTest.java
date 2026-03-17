530e1b1195963e6e6e7e9fd5729dc45fd6ebc1ee
noear <noear@live.cn>
2026-03-17 20:10:33 +0800

    添加 测试

---
+++ soloncode-web/src/test/java/features/bot/codeweb/WebTest.java
@@ -0,0 +1,15 @@
+package features.bot.codeweb;
+
+import org.noear.solon.bot.codecli.App;
+
+/**
+ *
+ * @author noear 2026/3/16 created
+ *
+ */
+public class WebTest {
+    public static void main(String[] args) {
+        // 下载 skills： https://github.com/solonlab/opencode-skills
+        App.main(args);
+    }
+}
