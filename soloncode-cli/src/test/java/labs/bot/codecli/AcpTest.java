1dc61cf3e048240b3a6dc48a1bb4b02ef508b8e8
noear <noear@live.cn>
2026-05-04 16:02:03 +0800

    添加 AcpTest

---
+++ soloncode-cli/src/test/java/labs/bot/codecli/AcpTest.java
@@ -0,0 +1,14 @@
+package labs.bot.codecli;
+
+import org.noear.solon.codecli.App;
+
+/**
+ *
+ * @author noear 2026/4/18 created
+ *
+ */
+public class AcpTest {
+    public static void main(String[] args) {
+        App.main(new String[]{"acp"});
+    }
+}
