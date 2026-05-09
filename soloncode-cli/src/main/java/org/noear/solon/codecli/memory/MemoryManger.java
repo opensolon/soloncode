66edf030062a0399b34e14989545b7d448064d01
noear <noear@live.cn>
2026-05-09 20:49:00 +0800

    优化 md 记忆方案类

--- soloncode-cli/src/main/java/org/noear/solon/codecli/memory/MemoryManger.java
+++ soloncode-cli/src/main/java/org/noear/solon/codecli/memory/MemoryManger.java
@@ -1,8 +1,10 @@
 package org.noear.solon.codecli.memory;
 
 import org.noear.solon.ai.skills.memory.MemorySolution;
+import org.noear.solon.ai.skills.memory.md.MemorySolutionMdImpl;
 import org.noear.solon.codecli.config.AgentProperties;
 
+import java.nio.file.Paths;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -20,6 +22,8 @@ public class MemoryManger implements MemorySolution.Factory {
             __cwd = properties.getUserHome();
         }
 
-        return cached.computeIfAbsent(__cwd, k -> new MemorySolutionImpl(k, properties));
+
+        return cached.computeIfAbsent(__cwd, k ->
+                new MemorySolutionMdImpl(Paths.get(k, properties.getHarnessMemory())));
     }
 }
