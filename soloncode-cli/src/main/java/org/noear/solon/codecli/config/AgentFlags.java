8803bc9a0e53ecfeb71e0bc6dc8e14507ba6d488
noear <noear@live.cn>
2026-05-15 10:48:55 +0800

    v2026.5.15

--- soloncode-cli/src/main/java/org/noear/solon/codecli/config/AgentFlags.java
+++ soloncode-cli/src/main/java/org/noear/solon/codecli/config/AgentFlags.java
@@ -24,7 +24,7 @@ public class AgentFlags {
     public final static String FLAG_WEB = "web";
 
     public static String getVersion() {
-        return "v2026.5.13";
+        return "v2026.5.15";
     }
 
     private static String lastVersion;
